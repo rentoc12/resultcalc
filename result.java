3e6f3d53420935d443001533eb18b5854add1ffd
Renato Silva <Renatoef79@gmail.com>
2025-01-16 12:20:50 -0300

    realizando alteração de resultados

--- result.java
+++ result.java
@@ -2,17 +2,17 @@ public class result {
 
     public static void main(String args[]){
 
-        double mat = 6;
-        double geo = 2;
-        double art = 2;
-        double port = 2;
+        double mat = 5;
+        double geo = 5;
+        double art = 5;
+        double port = 4;
         double resultCalc = mat + geo + art + port;
         double resultFinish = resultCalc / 4;
         System.out.println(resultFinish);
 
         if(resultFinish >= 7 ){
             System.out.println("Parabéns, você está aprovado");
-        }else if(resultFinish == 6){
+        }else if(resultFinish >= 5){
             System.out.println("Você ficará de recuperação");
         }
         else{
