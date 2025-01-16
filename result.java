4a1497c5ab6a01108fb85b33e99524756f5f12ea
Renato Silva <Renatoef79@gmail.com>
2025-01-16 11:50:40 -0300

    alterando else

--- result.java
+++ result.java
@@ -2,17 +2,20 @@ public class result {
 
     public static void main(String args[]){
 
-        double mat = 8;
-        double geo = 6;
-        double art = 9;
-        double port = 7;
+        double mat = 6;
+        double geo = 2;
+        double art = 2;
+        double port = 2;
         double resultCalc = mat + geo + art + port;
         double resultFinish = resultCalc / 4;
-        System.out.println(resulFinish);
+        System.out.println(resultFinish);
 
         if(resultFinish >= 7 ){
             System.out.println("Parabéns, você está aprovado");
-        }else(resultFinish <= 6){
+        }else if(resultFinish == 6){
+            System.out.println("Você ficará de recuperação");
+        }
+        else{
             System.out.println("Reprovado, tente novamente!!!");
         }
     }
